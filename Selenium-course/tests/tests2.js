401cbafad85eaa3a37726ca95b23b5c4d3380b32
larnaudie <pablolarnaudie29@gmail.com>
2023-03-26 23:28:33 -0300

    update
    Creamos un nuevo caso de prueba en base a un sign in a github, tiene un error de codigo al ejecutar:
    
    PS C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos Gloabnt\My-Javascript-practices\Selenium-course\tests> node tests2.js
    
    DevTools listening on ws://127.0.0.1:65484/devtools/browser/59547c2c-4d35-42eb-9739-de001765b5c0
    C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos Gloabnt\My-Javascript-practices\Selenium-course\node_modules\selenium-webdriver\lib\error.js:524
        let err = new ctor(data.message)
                  ^
    
    NoSuchElementError: no such element: Unable to locate element: {"method":"partial link text","selector":"Sign in"}
      (Session info: chrome=111.0.5563.111)
        at Object.throwDecodedError (C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos Gloabnt\My-Javascript-practices\Selenium-course\node_modules\selenium-webdriver\lib\error.js:524:15)
        at parseHttpResponse (C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos Gloabnt\My-Javascript-practices\Selenium-course\node_modules\selenium-webdriver\lib\http.js:587:13)
        at Executor.execute (C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos Gloabnt\My-Javascript-practices\Selenium-course\node_modules\selenium-webdriver\lib\http.js:515:28)
        at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
        at async Driver.execute (C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos Gloabnt\My-Javascript-practices\Selenium-course\node_modules\selenium-webdriver\lib\webdriver.js:745:17)
        at async toWireValue (C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos Gloabnt\My-Javascript-practices\Selenium-course\node_modules\selenium-webdriver\lib\webdriver.js:145:15)
        at async C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos Gloabnt\My-Javascript-practices\Selenium-course\node_modules\selenium-webdriver\lib\webdriver.js:195:16
        at async forEachKey (C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos Gloabnt\My-Javascript-practices\Selenium-course\node_modules\selenium-webdriver\lib\webdriver.js:189:9)
        at async convertKeys (C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos Gloabnt\My-Javascript-practices\Selenium-course\node_modules\selenium-webdriver\lib\webdriver.js:194:3)
        at async Driver.execute (C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos Gloabnt\My-Javascript-practices\Selenium-course\node_modules\selenium-webdriver\lib\webdriver.js:743:22) {
      remoteStacktrace: 'Backtrace:\n' +
        '\t(No symbol) [0x0044DCE3]\n' +
        '\t(No symbol) [0x003E39D1]\n' +
        '\t(No symbol) [0x002F4DA8]\n' +
        '\t(No symbol) [0x0032019F]\n' +
        '\t(No symbol) [0x003203AB]\n' +
        '\t(No symbol) [0x0034EE62]\n' +
        '\t(No symbol) [0x0033AF14]\n' +
        '\t(No symbol) [0x0034D57C]\n' +
        '\t(No symbol) [0x0033ACC6]\n' +
        '\t(No symbol) [0x00316F68]\n' +
        '\t(No symbol) [0x003180CD]\n' +
        '\tGetHandleVerifier [0x006C3832+2506274]\n' +
        '\tGetHandleVerifier [0x006F9794+2727300]\n' +
        '\tGetHandleVerifier [0x006FE36C+2746716]\n' +
        '\tGetHandleVerifier [0x004F6690+617600]\n' +
        '\t(No symbol) [0x003EC712]\n' +
        '\t(No symbol) [0x003F1FF8]\n' +
        '\t(No symbol) [0x003F20DB]\n' +
        '\t(No symbol) [0x003FC63B]\n' +
        '\tBaseThreadInitThunk [0x767A00F9+25]\n' +
        '\tRtlGetAppContainerNamedObjectPath [0x77967BBE+286]\n' +
        '\tRtlGetAppContainerNamedObjectPath [0x77967B8E+238]\n'
    }
    
    Node.js v18.15.0
    PS C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos Gloabnt\My-Javascript-practices\Selenium-course\tests> [17008

--- Selenium-course/tests/tests2.js
+++ Selenium-course/tests/tests2.js
@@ -1,15 +1,54 @@
 const { By, Key, Builder } = require("selenium-webdriver");
 require("chromedriver");
-async function test_case() {
+
+async function test_case2() {
   let driver = await new Builder().forBrowser("chrome").build();
 
-  await driver.get("https://google.com");
+  /**Aca estamos accediendo a github, el resto es como tests2.js */
+  await driver.get("https://github.com");
 
-  await driver.findElement(By.name("q")).sendKeys("Hello World!", Key.RETURN);
+  /**Aca empezamos a buscar el elemento Sign In mediante una busqueda partialLinkText que
+   * lo que hace es buscar un string que contenga ese string, y cuando lo encuentra le da click
+   **/
+  await driver.findElement(By.partialLinkText("Sign in")).click();
 
-  setInterval(function () {
-    driver.quit();
-  }, 10000);
+  /** Una vez que hizo click en el boton nos enviara a una nueva pagina, por lo que obtendremos
+   *su titulo al que tambien vamos a mostrar ese titulo en la consola.*/
+  console.log(await driver.getTitle());
+
+  /**Ahora vamos a testear ese titulo, asi que si ese titulo es el que esperamos que sea
+   * le vamos a decir que imprima en la consola; "test #q success" sino "failed"
+   */
+  if ((await driver.getTitle()) === "Sign in to GitHub") {
+    console.log("Test #1 success");
+  } else {
+    console.log("Test #1 failed");
+    return;
+  }
+
+  /* Ahora vamos a buscar dos elementos mas por su nombre! en este caso el nombre login y cuando lo encuentre escribira SteamCode
+(que refiere al nombre de la pagina de youtube el tutorial, le puedo decir que escriba pablolarnaudie)*/
+  await driver.findElement(By.name("login")).sendKeys("SteamCode");
+  /**Aca hara lo mismo pero con la contraseña, le escribiremos asteriscos, una vez que escriba steamcode y los astericos,
+   * le diremos que presione la tecla enter (key.return)*/
+  await driver.findElement(By.name("password")).sendKeys("****", Key.RETURN);
+
+  /**Aca la conicion indica que si en el driver encuentra el elemento por el nombre de clase "flash-close js-flash-close" y se muestra (isDisplayed()
+   * entonces le diremos que imprima en la consola que fue satisfactorio el test case.
+   * ACALARACION:
+   * Para saber el nombre de className exacto, podremos ir a la pagina web, inspect element, buscar en html el nombre de la clase
+   * que nos interesa.
+   * */
+
+  if (
+    await driver
+      .findElement(By.className("flash-close js-flash-close"))
+      .isDisplayed()
+  ) {
+    console.log("Test #2 success");
+  }
+
+  driver.quit();
 }
 
-test_case();
+test_case2();
