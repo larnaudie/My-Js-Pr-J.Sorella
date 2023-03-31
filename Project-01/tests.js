3512df2f19980070889b145a7eb0c7ec26ab8a19
larnaudie <pablolarnaudie29@gmail.com>
2023-03-30 23:22:45 -0300

    update
    Funciono test case de testeo pero fallo hacer click en login

--- Project-01/tests.js
+++ Project-01/tests.js
@@ -1,25 +1,37 @@
+/**Lo primero que hicismo fue abrir el visual studio code,
+ * Luego abrimos la carpeta donde queremos hacer el tutorial, yo la llame selenium curse
+ * Luego adentro de ella llamamos a la terminal ctl+shift+p,
+ * Alli lo primero que hicimos fue instalar node js, npm y luego selenium
+ * Para instalar el node JS priemero vamos a su pagina web y lo descargamos e instalamos,
+ * luego instalamos el npm escribienod en la terminal: npm init -y, lo que hace esl inicializar 
+  npm y a lo que pregunte le damos -y que es yes, vemos que se agrega el package.json,
+ * Luego vamos a instalar el selenium-driver y el chrome driver, el gekodriver es para firefox, 
+  asique en la terminal escribimos: npm install --save selenium-webdriver chrome driver,
+ *Vamos a ver como se nos agrega una carpeta node_modules, son modulos que vamos a estar usando en el codigo.
+ *vamos a crear una carepta tests y dentro un archivo test.js
+ *Luego en la terminal escribiremos cd tests pera setear el camino de la carpeta.
+ *Ahora vamos a escribir codigo:
+ */
 const { By, Key, Builder } = require("selenium-webdriver");
 require("chromedriver");
 
 async function project_1() {
   let driver = await new Builder().forBrowser("chrome").build();
 
-  /**When I access to the Demo Blaze home page*/
+  //When I access to the Demo Blaze home page
   await driver.get("https://www.demoblaze.com/");
 
-  /**Then I should see a Log In button in the navigation bar*/
+  //Then I should see a Log In button in the navigation bar
   await driver.findElement(By.id("login2"));
 
-  if (
-    await driver.findElement(By.id("login2")) //Me da failed
-  ) {
+  if (await driver.findElement(By.id("login2"))) {
     console.log("Test #1 success, I should see a Log In button");
   } else {
     console.log("Test #1 failed, I can not see a Log In button");
     return;
   }
 
-  /** And I should see a Sign Up button in the navigation bar*/
+  // And I should see a Sign Up button in the navigation bar
   await driver.findElement(By.id("signin2"));
 
   if (await driver.findElement(By.id("signin2"))) {
@@ -29,11 +41,11 @@ async function project_1() {
     return;
   }
 
-  /*When I click over the Log In button in the navigation bar*/
+  //When I click over the Log In button in the navigation bar
 
   await driver.findElement(By.id("login2")).click();
 
-  /**Then I should see a Log In pop up */
+  //Then I should see a Log In pop up
   if (await driver.findElement(By.className("modal-content"))) {
     //Me da failed
     console.log("Test #3 success, I click over the Log In button");
@@ -42,24 +54,26 @@ async function project_1() {
     return;
   }
 
-  /** When I complete the user name field with an existing user name*/
+  // When I complete the user name field with an existing user name
   const user1 = await driver
     .findElement(By.id("loginusername"))
     .sendKeys("test");
-  /*And I complete the password field with the expected valid password*/
+  //And I complete the password field with the expected valid password
   const pass1 = await driver
     .findElement(By.id("loginpassword"))
     .sendKeys("test");
-  /**And I click over the Log In button in the Log In popup */
+  //And I click over the Log In button in the Log In popup
+
+  /*
   const log1 = await driver.findElement(By.partialLinkText("Log in")).click();
 
-  /**And I complete the password field with the expected valid password */
+  //And I complete the password field with the expected valid password
   if ((user1 && pass1 && log1) === true) {
     console.log("Test #7 success, User can access");
   } else {
     console.log("Test #7 failed, User can not access");
   }
-  /*Then I should not longer see any popup on the screen*/
+  //Then I should not longer see any popup on the screen
   await driver.findElement(By.id("login2"));
 
   if (await driver.findElement(By.id("login2"))) {
@@ -68,7 +82,7 @@ async function project_1() {
     console.log("Test #8 failed, I should see a Log In button");
     return;
   }
-  /*And I should not longer see the Sign Up button in the navigation bar*/
+  //And I should not longer see the Sign Up button in the navigation bar
   await driver.findElement(By.id("signin2"));
 
   if (await driver.findElement(By.id("signin2"))) {
@@ -78,7 +92,7 @@ async function project_1() {
     return;
   }
 
-  /**And I should see a "Welcome <username>" button in the navigation bar */
+  //And I should see a "Welcome <username>" button in the navigation bar
   await driver.findElement(By.id("nameofuser"));
   if (await driver.findElement(By.id("nameofuser"))) {
     console.log("Test #10 success, I am in");
@@ -86,6 +100,6 @@ async function project_1() {
     console.log("Test #10 failed, I am not in");
     return;
   }
+  */
 }
-
 project_1();
