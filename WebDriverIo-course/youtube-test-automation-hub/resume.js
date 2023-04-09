e41c6f77dfc91a254bbee0def6c8232ba952641c
larnaudie <pablolarnaudie29@gmail.com>
2023-04-09 20:46:04 -0300

    Fin del curso de youtube
    Complete el curso rapido de webdriverio mocha con javascript de youtube!

--- WebDriverIo-course/youtube-test-automation-hub/resume.js
+++ WebDriverIo-course/youtube-test-automation-hub/resume.js
@@ -2,4 +2,8 @@
 Primero instalamos npm init
 Luego instalamos npm i @wdio/cli
 Luego instalamos npx wdio config
+
+En este curso corto lo que haremos será crear un nuevo archivo llamado login.specs.js 
+dentro de la carpeta test/specs, se puede llamar login.specs o como querramos.
+allí seguiremos explicando.
  */
