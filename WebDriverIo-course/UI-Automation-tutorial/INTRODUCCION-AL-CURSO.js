acacc1985278da92b08ebc858feeab632c504b26
larnaudie <pablolarnaudie29@gmail.com>
2023-04-09 19:17:04 -0300

    Modificando
    al instalar la configuracion de wdio habian errores, se reinstalaron y se instalaron al igual que en el video del chpater 2.1 de TAU

--- WebDriverIo-course/UI-Automation-tutorial/INTRODUCCION-AL-CURSO.js
+++ WebDriverIo-course/UI-Automation-tutorial/INTRODUCCION-AL-CURSO.js
@@ -6,39 +6,37 @@ npm i @wdio/cli --save : es para instalar y configurar el framework.
 npx wdio config : Esto nos instalara y configurara la instalacion del wdio con preguntas.
 
 ---------------------------------------------------------------------------------------------------------------------------------
-
 ===============================
 🤖 WDIO Configuration Wizard 🧙
-===============================                                                                       
+===============================
 
-? A project named "ui-automation-tutorial" was detected at
-"C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos
-Gloabnt\My-Javascript-practices\WebDriverIo-course\UI-Automation-tutorial", correct? Yes      
-? Where should your tests be launched? browser - for unit and component testing in the browser  
-                                                                                                rsos  
-? Which framework do you use for building components? Other
+? A project named "ui-automation-tutorial" was detected at 
+"C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos 
+Gloabnt\My-Javascript-practices\WebDriverIo-course\UI-Automation-tutorial", correct? Yes
+? Where should your tests be launched? local - for e2e testing of web and mobile applications
 ? Where is your automation backend located? On my local machine
 ? Which framework do you want to use? Mocha (https://mochajs.org/)
 ? Do you want to use a compiler? No!
-? Do you want WebdriverIO to autogenerate some test files? Yes
-? Where should these files be located? (C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cu
-? Where should these files be located? 
-C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos                                    
-Gloabnt\My-Javascript-practices\WebDriverIo-course\UI-Automation-tutorial\src\**\*.test.js    
+? Do you want WebdriverIO to autogenerate some test files? Yes */
+//? Where should these files be located? ./test/specs/**/*.js
+/*? Do you want to use page objects (https://martinfowler.com/bliki/PageObject.html)? Yes
+? Where are your page objects located? C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos 
+Gloabnt\My-Javascript-practices\WebDriverIo-course\UI-Automation-tutorial\test\pageobjects\**\*.js  
 ? Which reporter do you want to use? spec
 ? Do you want to add a plugin to your test setup?
 ? Do you want to add a service to your test setup? chromedriver
+? What is the base url? http://localhost    
 ? Do you want me to run `npm install` Yes
 
 --------------------------------------------------------------------------------------------------------
-NOS CREA UNA CARPETA SRC, CON UN ARCHIVO COMPONENT.TEST.JS
+
+NOS CREA UNA CARPETA test, CON dos carpetas pageobjects y specs, alli tendremos nuestros archivos para principantes y entender como funciona webdriverio
 NOS CREA UN ARCHIVO SUELTO LLAMADO WDIO.CONF.JS
 UNA VEZ QUE SE INSTALA NOS DICE:
 
-🤖 Successfully setup project at C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos 
-Gloabnt\My-Javascript-practices\WebDriverIo-course\UI-Automation-tutorial 🎉
+🤖 Successfully setup project at C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos Gloabnt\My-Javascript-practices\WebDriverIo-course\UI-Automation-tutorial 🎉
 
-Join our Matrix community and instantly find answers to your issues or queries. Or just join and say hi 👋!
+Join our Matrix community and instantly find answers to your issues or queries. Or just join and say hi 👋! 
   🔗 https://matrix.to/#/#webdriver.io:gitter.im
 
 Visit the project on GitHub to report bugs 🐛 or raise feature requests 💡:
@@ -46,9 +44,9 @@ Visit the project on GitHub to report bugs 🐛 or raise feature requests 💡:
 
 To run your tests, execute:
 $ cd C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos Gloabnt\My-Javascript-practices\WebDriverIo-course\UI-Automation-tutorial
-$ npm run wdio    ---> ella usa npx wdio run wdio.conf.js
+$ npm run wdio
 
-PS C:\Users\pablo\OneDrive\Desktop\Estudios\Programacion\Cursos Gloabnt\My-Javascript-practices\WebDriverIo-course\UI-Automation-tutorial>
+ -------------------------------------------------------------------------------------------------------------------------
 
 IMPORTANTE, en la version de webdriverio 7 viene instalado el uso de la asincronia
 si estas usando una version del node.js 16 o superior (tenemos 18 de momento) chrome no
