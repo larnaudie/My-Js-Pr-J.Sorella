25b915a82b712732e7ce359c0ff35280cdfbbce6
larnaudie <pablolarnaudie29@gmail.com>
2023-05-22 23:33:29 -0300

    Ahora si, archivo pronto
    Ahora comienzo a configurar todo para las correcciones.

--- Project-01-webdriverio/wdio.conf.js
+++ Project-01-webdriverio/wdio.conf.js
@@ -22,7 +22,7 @@ exports.config = {
   // then the current working directory is where your `package.json` resides, so `wdio`
   // will be called from there.
   //
-  specs: ["./test/specs/**/project-01"],
+  specs: ["./test/specs/**/project-01.js"],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
