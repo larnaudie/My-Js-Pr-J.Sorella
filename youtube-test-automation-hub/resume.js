f47723c455878755bb7f0eb8728299ee3d86f2e9
Pablo Larnaudie <pablolarnaudie29@gmail.com>
2023-07-02 17:51:10 -0300

    comenzando
    mis practicas con javascript

---
+++ youtube-test-automation-hub/resume.js
@@ -0,0 +1,9 @@
+/*
+Primero instalamos npm init
+Luego instalamos npm i @wdio/cli
+Luego instalamos npx wdio config
+
+En este curso corto lo que haremos será crear un nuevo archivo llamado login.specs.js 
+dentro de la carpeta test/specs, se puede llamar login.specs o como querramos.
+allí seguiremos explicando.
+ */
