2ddfc6c55e3fc1808bd2a4afbe80f3bd865c83ed
Pablo Larnaudie <pablolarnaudie29@gmail.com>
2023-07-02 20:50:23 -0300

    Comenzando - mis pruebas de javascript

--- My-Js-trys-cypress/udy-course/cypress/integration/examples/Test6.js
+++
@@ -1,8 +0,0 @@
-describe("My Third test suite", () => {
-  it("My ThirdTest case", () => {
-    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
-    cy.get("div.mouse-hover-content").invoke("show");
-    cy.contains("Top").click();
-    cy.url().should("include", "top");
-  });
-});
--- My-Js-trys-cypress/udy-course/cypress/integration/pageObjects/ProductPage.js
+++
@@ -1,6 +0,0 @@
-class ProductPage {
-   checkOutBtn(){
-        return cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link')
-    }
-    }
-    export default ProductPage
